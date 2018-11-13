940a65d931cbda3c52a9361aefcac7b90bcb7ae4
dx <704423953@qq.com>
2018-11-13 11:05:06 +0800

    Data for maachine learing and the linear learn

---
+++ temp.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+x,y=[],[]
+for sample in open("_Data\prices.txt","r"):
+     _x,_y=sample.split(",")
+     x.append(float(_x))
+     y.append(float(_y))
+x,y=np.array(x),np.array(y)
+x=(x-x.mean())/x.std()
+plt.figure()
+plt.scatter(x,y,c="r",s=5)
+plt.show()
+x0=np.linspace(-2,4,100)
+def get_model(deg):
+    return lambda input_x=x0:np.polyval(np.polyfit(x,y,deg),input_x)
+def get_cost(deg,input_x,input_y):
+    return 0.2*((get_model(deg)(input_x)-input_y)**2).sum()
+test_set=[1,4,10]
+for d in test_set:
+    print(get_cost(d,x,y))
+plt.scatter(x,y,c="g",s=20)
+for d in test_set:
+    plt.plot(x0,get_model(d)(),label="degree = {}".format(d))
+plt.xlim(-2,4)
+plt.ylim(1e5,8e5)
+plt.legend()
+plt.show()
+
